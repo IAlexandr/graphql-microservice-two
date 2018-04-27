568afdcab2fa6e9da0539b4cf25c0520ea4caf96
Alexander Ivanov <aivanov@informatica.ru>
2018-04-27 10:17:31 +0300

    redis connection

--- pubsub.js
+++ pubsub.js
@@ -1,8 +1,25 @@
-const { RedisPubSub } = require('graphql-redis-subscriptions');
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis from 'ioredis';
+
+const options = {
+  port: 32768,
+  retryStrategy: options => {
+    return Math.max(options * 100, 3000);
+  },
+  showFriendlyErrorStack: process.env.NODE_ENV !== 'production',
+};
+const redis = new Redis(options);
+
+redis.on('connect', () => {
+  console.log('Redis connection port', options.port);
+});
+redis.on('error', error => {
+  console.log('Redis connection error', error);
+});
 
 const pubsub = new RedisPubSub({
-  connection: { port: 32768 },
+  publisher: redis,
+  subscriber: redis,
 });
-console.log('redis connection port ', 32768);
 
 module.exports = pubsub;
